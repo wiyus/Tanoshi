5e8277c373306933eb609d65eeef77c984b464fc
wiyus <110345384+wiyus@users.noreply.github.com>
2025-06-04 21:09:42 +0200

    First commit

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace TanoshiBot;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+    
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        
+        var token = File.ReadAllText("../../../token.txt");
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+}
